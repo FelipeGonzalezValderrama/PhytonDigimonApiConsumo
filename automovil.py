ec139d73917893c6d876644df34d0216f1ae6178
Felipe Gonzalez <felipe.gonzalez.valderrama@gmail.com>
2023-05-20 18:08:55 -0400

    delete de archivos cambio nombre folder proyecto

--- automovil.py
+++
@@ -1,9 +0,0 @@
-
-from vehiculo import Vehiculo
-
-
-class Automovil(Vehiculo):
-    def __init__(self, marca, modelo, ruedas, velocidad, cilindrada):
-        super().__init__(marca, modelo, ruedas)
-        self.velocidad = velocidad
-        self.cilindrada = cilindrada
--- vehiculo.py
+++
@@ -1,5 +0,0 @@
-class Vehiculo:
-    def __init__(self, marca, modelo, ruedas):
-        self.marca = marca
-        self.modelo = modelo
-        self.ruedas = ruedas
